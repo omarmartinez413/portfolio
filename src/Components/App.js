81bc3dc99bad96397f222b1fe99f6b2813dc3d7c
omarmartinez413 <Omarmartinez@Omars-Air.home>
2019-01-03 22:48:58 -0500

    seperated banner and header components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import Header from './Header'
+import Header from './Header';
+import Banner from './Banner';
 
 class App extends Component {
 	render() {
-	  return <Header/>;
+	  return (
+		<Header/>,
+		<Banner/>
+		)
   }
 }
 
